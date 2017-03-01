b613b70e3b1627c78e9c0f6baf25d24531706291
Becca Harris <bharris@providigm.com>
2017-02-28 22:56:37 -0700

    new app stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,63 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }      from '@angular/platform-browser';
+import { NgModule }           from '@angular/core';
+import { FormsModule }        from '@angular/forms';
+import { HttpModule }         from '@angular/http';
+import { RouterModule }       from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { AppComponent }       from './app.component';
+import { HeaderComponent }    from './header/header.component';
+import { AdoptionComponent }  from './adoption/adoption.component';
+import { EventsComponent }    from './events/events.component';
+import { ContactComponent }   from './contact/contact.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { DashComponent }      from './dash/dash.component';
+import { DogsComponent } from './dogs/dogs.component';
+import { CatsComponent } from './cats/cats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    HeaderComponent,
+    AdoptionComponent,
+    EventsComponent,
+    ContactComponent,
+    ResourcesComponent,
+    DashComponent,
+    DogsComponent,
+    CatsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'dash',
+        component: DashComponent
+      },{
+        path: 'adoption',
+        component: AdoptionComponent
+      },{
+        path: 'events',
+        component: EventsComponent
+      },{
+        path: 'resources',
+        component: ResourcesComponent
+      },{
+        path: 'contact',
+        component: ContactComponent
+      },{
+        path: 'adoption/dogs',
+        component: DogsComponent
+      },{
+        path: 'adoption/cats',
+        component: CatsComponent
+      },{
+        path: '',
+        redirectTo: '/dash',
+        pathMatch: 'full'
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/resources/resources.component.spec.ts
+++ src/app/resources/resources.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavComponent } from './nav.component';
+import { ResourcesComponent } from './resources.component';
 
-describe('NavComponent', () => {
-  let component: NavComponent;
-  let fixture: ComponentFixture<NavComponent>;
+describe('ResourcesComponent', () => {
+  let component: ResourcesComponent;
+  let fixture: ComponentFixture<ResourcesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
+      declarations: [ ResourcesComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavComponent);
+    fixture = TestBed.createComponent(ResourcesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
