788cb07b0452adf9ca72a2314c21bbf33401bcc9
Becca Harris <bharris@providigm.com>
2017-03-07 15:01:01 -0700

    buttons and routing

--- src/app/adoption/adoption.component.ts
+++ src/app/adoption/adoption.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router'
+
+import { AppRoutingModule }  from '../app-routing.module'
 
 @Component({
   selector: 'app-adoption',
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdoptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  gotoDogs() {
+    this.router.navigate(['/adoption/dogs']);
+  }
+
+  gotoCats() {
+    this.router.navigate(['/adoption/cats']);
+  }
 
   ngOnInit() {
   }
