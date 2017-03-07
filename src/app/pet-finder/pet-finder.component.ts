a8ec2896256fc3f2a72fd75934fe585d48c903b5
Becca Harris <bharris@providigm.com>
2017-03-06 23:30:12 -0700

    starting with forms

--- src/app/pet-finder/pet-finder.component.ts
+++ src/app/pet-finder/pet-finder.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Pet_Finder } from './pet-finder';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-pet-finder',
   templateUrl: './pet-finder.component.html',
   styleUrls: ['./pet-finder.component.css']
 })
-export class PetFinderComponent implements OnInit {
+export class PetFinderComponent {
 
-  constructor() { }
+  species = ['Any', 'Dog', 'Cat', 'Small Mammal', 'Horse'];
 
-  ngOnInit() {
-  }
+  model = new Pet_Finder(18, 'Dr IQ', this.species[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+
+  get diagnostic() { return JSON.stringify(this.model);}
 
 }
