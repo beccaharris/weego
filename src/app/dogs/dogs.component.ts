a10ed63764ab5d53942511292ec91da6aff24729
Becca Harris <bharris@providigm.com>
2017-03-09 16:09:59 -0700

    array

--- src/app/dogs/dogs.component.ts
+++ src/app/dogs/dogs.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
+export class Dogs {
+  id: number;
+  name: string;
+  breed: string;
+  gender: string;
+  age: string;
+  color: string;
+  location: string
+}
+
 @Component({
   selector: 'app-dogs',
   templateUrl: './dogs.component.html',
@@ -11,6 +21,9 @@ export class DogsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const DOGS: Dogs [] = [
+      {id: 1, name: 'Rufus', breed: 'German Shepherd Dog and Australian Cattle Dog', gender: 'Male - 51 pounds', age: '02 Years 02 Months', color: 'Red color', location:'Denver Dumb Friends League'}
+    ]
   }
 
 }
