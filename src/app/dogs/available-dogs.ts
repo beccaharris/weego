a92f51f3313d1c4ed023e10b915bacf8fedbc55b
Becca Harris <bharris@providigm.com>
2017-03-25 23:19:19 -0600

    adding more dogs

--- src/app/dogs/available-dogs.ts
+++ src/app/dogs/available-dogs.ts
@@ -1,7 +1,27 @@
 import { Dog } from './dog';
 
 export const Dogs: Dog [] = [
-  { picture:'../assets/images/dogs/rufus.png', name:'Rufus', breed: 'German Shepherd Dog and Australian Cattle Dog', gender: 'Male - 51 pounds', age: '02 Years 03 Months', color: 'Red color', location: 'Denver Dumb Friends League',
+  { picture:'../assets/images/dogs/marley.png', name:'Marley', breed: 'German Shepherd dog (Purebred)', gender: 'Female - 56 pounds', age: '04 Years 06 Months', color: 'Brown color', location: 'Denver Dumb Friends League',
+    tagline: 'DASHING A VERY POLITE',
+    favorites: 'Enjoys scratches and being well-behaved. She likes to play with toys!',
+    specialFeatures: 'Easy to leash and walk. She appears to be housebroken. Good energy and very gentle girl.',
+    dreamHome: 'A big back yard with an active owner to go on walks and provide love and attention.'
+  },{ picture:'../assets/images/dogs/snow.png', name:'Snow', breed: 'Labrador Retriever', gender: 'Female - 55 pounds', age: '07 Years 00 Months', color: 'White color', location: 'Denver Dumb Friends League',
+    tagline: '',
+    favorites: 'Stay tuned! This info is coming soon...',
+    specialFeatures: 'Stay tuned! This info is coming soon...',
+    dreamHome: 'Stay tuned! This info is coming soon...'
+  },{ picture:'../assets/images/dogs/bear.png', name:'Bear', breed: 'Australian Kelpie mix', gender: 'Male - 48 pounds', age: '05 Years 00 Months', color: 'Black color', location: 'Denver Dumb Friends League',
+    tagline: '',
+    favorites: 'Stay tuned! This info is coming soon...',
+    specialFeatures: 'Stay tuned! This info is coming soon...',
+    dreamHome: 'Stay tuned! This info is coming soon...'
+  },{ picture:'../assets/images/dogs/gandalf.png', name:'Gandalf', breed: 'Alaskan Malamute', gender: 'Male - 81 pounds', age: '06 Years 00 Months', color: 'Sable color', location: 'Denver Dumb Friends League',
+    tagline: '',
+    favorites: 'Stay tuned! This info is coming soon...',
+    specialFeatures: 'Stay tuned! This info is coming soon...',
+    dreamHome: 'Stay tuned! This info is coming soon...'
+  },{ picture:'../assets/images/dogs/rufus.png', name:'Rufus', breed: 'German Shepherd Dog and Australian Cattle Dog', gender: 'Male - 51 pounds', age: '02 Years 03 Months', color: 'Red color', location: 'Denver Dumb Friends League',
     tagline: 'HIGH-ENERGY SWEETHEART!',
     favorites: 'Long walks and affection',
     specialFeatures: 'High-energy, loving in shelter. Dog is being treated for heartworm, which requires activity limits and ongoing meds; please ask about his special needs.',
@@ -53,8 +73,38 @@ export const Dogs: Dog [] = [
     dreamHome: 'He is looking for a forever home with no kitties or small children. Dog buddies are OK!'
   },{ picture:'../assets/images/dogs/perla.png', name: 'Perla', breed: 'German Shepherd dog', gender: 'Female - 55 pounds', age: '02 Years 01 Months', color: 'Black and Tan color', location: 'MaxFund',
     tagline: 'PERFECT GIRL!',
+    favorites: 'Long walks and spending time with her favorite humans.',
+    specialFeatures: 'She is shy at first, but with a little encouragement and gentle handling, she warms up quickly and falls in love with her humans.',
+    dreamHome: 'Finley would love a home with a family that can be with her the majority of the time.'
+  },{ picture:'../assets/images/dogs/finley.png', name: 'Finley', breed: 'Labrador Retriever and Chow Chow mix', gender: 'Female - 43 pounds', age: '02 Years 03 Months', color: 'Tan color', location: 'MaxFund',
+    tagline: 'STUNNING LADY!',
     favorites: 'Kind humans, dog friends, and lots of adventures.',
     specialFeatures: 'Shy at first, but full of love once she warms up! A gentle girl that loves to lay around and relax, but is always up for an adventure with her people.',
     dreamHome: 'This girl would love a home with people who are patient with her while she warms up. She would enjoy a home with another dog buddy!'
+  },{ picture:'../assets/images/dogs/lambic.png', name: 'Lambic', breed: 'Chihuahua mix', gender: 'Male - 4 pounds', age: '02 Months', color: 'Tricolor color', location: 'MaxFund',
+    tagline: 'A REAL CUTIE!',
+    favorites: 'Playing, playing, playing, and snuggles.',
+    specialFeatures: 'Extremely loveable puppy that is good with all creatures.',
+    dreamHome: 'Lambic would love a home with humans that are ready to be patient with him through his puppyhood and provide a loving home dedicated to his well-being.'
+  },{ picture:'../assets/images/dogs/ana.png', name: 'Ana', breed: 'German Shepherd dog', gender: 'Female - 60 pounds', age: '02 Years 08 Months', color: 'Black and Tan color', location: 'Show Capped Shepherd Rescue',
+    tagline: 'WHAT A BEAUTY!',
+    favorites: 'Being active and going on adventures with her humans!',
+    specialFeatures: 'Ana is active, attentive, and affectionate. She is a little shy but warms up quickly!',
+    dreamHome: 'She would love a home that can encourage her and give her lots of love, as she came from a neglectful situation. She needs a family that is willing to work on her socialization with other pups to help her become less awkward.'
+  },{ picture:'../assets/images/dogs/evie.png', name: 'Evie', breed: 'Pug and Beagle mix', gender: 'Female - 30 pounds', age: '12 Years 01 Months', color: 'Tan color', location: 'Show Capped Shepherd Rescue',
+    tagline: 'A REAL SWEET GIRL',
+    favorites: 'Relaxing with her humans and going on leisurely walks.',
+    specialFeatures: 'A sweet girl that is friendly to everyone and everything.',
+    dreamHome: 'Needs a home to stay with her during her golden years. She would love a home that can provide physical activity to help keep her young!'
+  },{ picture:'../assets/images/dogs/cecil.png', name: 'Cecil', breed: 'Doberman Pinscher (purebred)', gender: 'Male - 60 pounds', age: '08 Years 01 Months', color: 'Black and Tan color', location: 'Doberman Rescue of Colorado',
+    tagline: 'DOBERGENT',
+    favorites: 'Hanging out with his favorite humans and taking it easy.',
+    specialFeatures: 'He is a very sweet and gentle boy with a timid personality. He gets along very well with adults and kids.',
+    dreamHome: 'He needs a home with humans that love him and are willing to spend a lot of time with him and give him lots of attention.'
+  },{ picture:'../assets/images/dogs/freeway.jpeg', name: 'Freeway', breed: 'Doberman Pinscher (purebred)', gender: 'Male - 55 pounds', age: '06 Years 03 Months', color: 'Black and Tan color', location: 'Doberman Rescue of Colorado',
+    tagline: 'SWEET, PATIENT GUY',
+    favorites: 'Hanging out with his favorite humans and taking it easy.',
+    specialFeatures: 'He is a very sweet and gentle boy with a timid personality. He loves to chase cats, so would do best in a home without them.',
+    dreamHome: 'He needs a home with humans that love him and are willing to spend a lot of time with him and give him lots of attention.'
   },
 ]
